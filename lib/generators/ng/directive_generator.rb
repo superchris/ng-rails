2d295f374330424ced7ab08b208ea142779cadcd
Jim Anders <github@janders223.com>
2013-07-07 21:34:48 -0400

    Add generator for directives

--- lib/generators/ng/directive_generator.rb
+++ lib/generators/ng/directive_generator.rb
@@ -2,18 +2,18 @@ require 'generators/ng/generator_helpers'
 
 module Ng
   module Generators
-    class ControllerGenerator < ::Rails::Generators::NamedBase
+    class DirectiveGenerator < ::Rails::Generators::NamedBase
       include Ng::Generators::GeneratorHelpers
       
       source_root File.expand_path("../../templates", __FILE__)
 
-      desc "Creates an AngularJS controller"
+      desc "Creates an AngularJS directive"
 
       class_option :js, type: :boolean, aliases: "--js", default: false, desc: "Use plain Javascript instead of Coffeescript"
       class_option :app, type: :string, aliases: "-n", default: false, desc: "Use a custom AngularJS application name"
 
       def create_controller_file
-        template "controller.#{extension}", "#{application_path}/controllers/#{file_name}_controller.#{extension}"
+        template "directive.#{extension}", "#{application_path}/directives/#{file_name}_directive.#{extension}"
       end
     end
   end
---
+++ test/generators/directive_generator_test.rb
@@ -0,0 +1,56 @@
+require 'test_helper'
+require 'generators/ng/directive_generator'
+
+class DirectiveGeneratorTest < Rails::Generators::TestCase
+  tests Ng::Generators::DirectiveGenerator
+
+  destination File.join(Rails.root, "tmp", "generator_test_output")
+
+  setup :prepare_destination
+
+  def copy_directory(dir)
+    source = Rails.root.join(dir)
+    dest = Rails.root.join("tmp", "generator_test_output", File.dirname(dir))
+
+    FileUtils.mkdir_p dest
+    FileUtils.cp_r source, dest
+  end
+
+  def prepare_destination
+    super
+
+    copy_directory "app/assets/javascripts"
+    copy_directory "app/views/layouts/application.html.erb"
+    copy_directory "config"
+  end
+
+  test "default generator creates coffee directive" do
+    run_generator ["datepicker"]
+    assert_file "#{angular_path}/#{application_name}/directives/datepicker_directive.coffee"
+  end
+
+  test "generator with --js flag creates javascript directive" do
+    run_generator ["datepicker", "--js"]
+    assert_file "#{angular_path}/#{application_name}/directives/datepicker_directive.js"
+  end
+
+  test "default generator with app name creates coffee directive" do
+    run_generator ["datepicker", "-n", "MyApp"]
+    assert_file "#{angular_path}/my_app/directives/datepicker_directive.coffee"
+  end
+
+  test "generator with --js flag and app name creates javascript directive" do
+    run_generator ["datepicker", "--js", "-n", "MyApp"]
+    assert_file "#{angular_path}/my_app/directives/datepicker_directive.js"
+  end
+
+  private
+
+  def application_name
+    "dummy"
+  end
+
+  def angular_path(custom_path = nil)
+   "app/assets/javascripts/#{custom_path}".chomp('/')
+  end
+end
